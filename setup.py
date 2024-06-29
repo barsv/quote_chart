3b9bf5a7ffa9f7b644373fa729c50c0d631676d1
stanislav.baranov <barsv85@gmail.com>
2024-06-29 13:21:55 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quote_chart',
-    version='0.1.2',
+    version='0.1.3',
     description='Customized dash/plotly chart for price candles.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
